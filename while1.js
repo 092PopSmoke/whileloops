93f4bca06ab0832b86813a8a93c42569cd3ea314
Lean <93644347+092PopSmoke@users.noreply.github.com>
2021-12-08 10:08:32 +0100

    Add files via upload

---
+++ while1.js
@@ -0,0 +1,8 @@
+var i = 1;
+while (i <= 50) {
+    if (i % 2 == 0) {
+        document.write(i + "<br>")
+    }
+    i++;
+}
+
---
+++ while2.js
@@ -0,0 +1,7 @@
+var a = 1;
+while (a <= 100) {
+    if ((a % 3 == 0) && (a % 5 == 0)) {
+        document.write("<br>" + a)
+    }
+    a++;
+}
